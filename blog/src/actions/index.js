f31db6daeb941bf04a16bf52f6cc496f572d212c
Levan Odishvili <lodishvili@vaba.co>
2018-08-15 10:14:19 +0400

    almost finished

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POSTS = 'create_post';
 
 export const HOST_API = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=Rendfold'
 
 export function fetchPosts () {
-    const request =  axios.get(`${HOST_API}/posts${API_KEY}`);
+    const request = axios.get(`${HOST_API}/posts${API_KEY}`);
 
     return {
         type: FETCH_POSTS,
+        payload: request
+    };
+}
+
+export function createPost (values) {
+    const request = axios.post(`${HOST_API}/posts${API_KEY}`, values);
+
+    return {
+        type: CREATE_POSTS,
         payload: request
     };
 }
--- blog/src/components/PostsNew.js
+++ blog/src/components/PostsNew.js
@@ -1,26 +1,88 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom';
+import { createPosts } from '../actions';
 
 class PostsNew extends Component {
     constructor (props) {
         super(props);
     }
 
-    componentDidMount () {
+    change () {
 
     }
 
-    render() {
+    renderField (field) {
+        const className = `form-group ${field.meta.touched && field.meta.error ? 'has-danger' : ''}`
+
         return (
-            <div>
-                Hello
-            </div>        
+            <div className={className}>
+                <label>{field.labelText}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    { ...field.input }
+                />
+                <div className="text-help">
+                    { field.meta.touched ? field.meta.error : ''}
+                </div>
+            </div>
+        )
+    }
+
+    onSumbit (values) {
+        this.props.createPosts(values);
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit((values) => this.onSumbit(values))}>
+                <Field 
+                    labelText="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field 
+                    labelText="Categories"
+                    name="categories"
+                    component={this.renderField}
+                />
+                <Field 
+                    labelText="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>      
         );
     }
 }
 
-function mapStateToProps (state) {
-    return { }
+function validate (values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+
+    if (!values.categories) {
+        errors.categories = "Enter a categories!";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter a post content!";
+    }
+
+    return errors;
 }
 
-export default connect(mapStateToProps, null)(PostsNew);
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(
+    connect(null, {createPosts})(PostsNew)
+);
